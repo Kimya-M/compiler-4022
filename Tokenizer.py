65abd7074f981a54bdeaa8e5d534d12cccace04b
Arshia Shafiei <arshiashafiei03@gmail.com>
2024-04-25 18:44:38 +0330

    add all needed functions

--- Tokenizer.py
+++ Tokenizer.py
@@ -1,7 +1,7 @@
 token_map = {
     "bool": "T_Bool",
     "break": "T_Break",
-    "char": "T_Char",
+    "char": "T_Char_keyword",
     "continue": "T_Continue",
     "else": "T_Else",
     "false": "T_False",
@@ -36,20 +36,38 @@ token_map = {
     ",": "T_Comma",    # Comma
 }
 
-def get_token_until_delsp(token: str) -> str:
+def get_token_until_delspop(token: str) -> str:
+    #print(token)
     index = 0
     for i in range(len(token)):
-        if is_whitespace(token[i]) or is_delimiter(token[i]):
+        if is_whitespace(token[i]) or is_delimiter(token[i])[0] or is_a_operator(token[i]):
+            #print(is_whitespace(token[i]),is_delimiter(token[i])[0],is_operator(token[i])[0])
             index = i
+            #print(token[:index],"hey")
             break
-
     return token[0:index]
+
+
+def is_a_operator(token:str):
+    if token[0] == '=' or token[0] == '+' or token[0] == '-' or token[0] == '*' or token[0] == '/' or token[0] == '%' or token[0] == '!' or token[0] == '<' or token[0] == '>':
+        return True
+    return False 
 
 def get_token_name(token: str):
     token_name = ""
     if token in token_map.keys():
         token_name = token_map[token]
     return token_name
+
+def is_hex(s: str):
+    if s.startswith("0X") or s.startswith("0x"):
+        try:
+            int(s[2:], 16)
+            return True
+        except ValueError:
+            return False
+    else:
+        return False
 
 #removinf spaces and comments
 def is_comment(token: str):
@@ -68,9 +86,11 @@ def is_comment(token: str):
     
 def is_identifier(token: str):
     """Checks if a token is a combination of letters, digits, or underscore."""
-    golabi = get_token_until_delsp(token)
-
-    if golabi[0] =='_' or golabi[0].isalpha():
+    golabi = get_token_until_delspop(token)
+    #print(golabi, "here", len(golabi))
+    if len(golabi) == 0:
+        return False, None
+    if golabi[0] == '_' or golabi[0].isalpha():
         for i in range(len(golabi)):
             if golabi[i] =='_' or golabi[i].isalnum():
                 continue
@@ -78,6 +98,7 @@ def is_identifier(token: str):
                 return False, None
         
         return True, golabi
+    return False, None
    
 
 def is_operator(token: str):
@@ -85,34 +106,35 @@ def is_operator(token: str):
 
     if token[0] == '=':
         if token[1] == '=':
-            return True, get_token_name(token[0:2])
-        return True, get_token_name(token[0])
+            return True, "=="
+        return True, "="
     elif token[0] == '<':
         if token[1] == '=':
-            return True, get_token_name(token[0:2])
-        return True, get_token_name(token[0])
+            return True, "<="
+        return True, "<"
     elif token[0] == '>':
         if token[1] == '=':
-            return True, get_token_name(token[0:2])
-        return True, get_token_name(token[0])
+            return True, ">="
+        return True, ">"
     elif token[0] == '!':
         if token[1] == '=':
-            return True, get_token_name(token[0:2])
-        return True, get_token_name(token[0])
+            return True, "!="
+        return True, "!"
     elif token[0] == '+':
-        return True, get_token_name(token[0])
+        return True, "+"
     elif token[0] == '-':
-        return True, get_token_name(token[0])
+        return True, "-"
     elif token[0] == '*':
-        return True, get_token_name(token[0])
+        return True, "*"
     elif token[0] == '/':
-        return True, get_token_name(token[0])
+        return True, "/"
     elif token[0] == '%':
-        return True, get_token_name(token[0])
+        return True, "%"
     elif token[0:1] == "&&":
-        return True, get_token_name(token[0:1])
-    elif token[0:1] == "||":
-        return True, get_token_name(token[0:1])
+        return True, "&&"
+    elif ord(token[0]) == 124 and ord(token[1]) == 124:
+        return True, "||"
+    return False, None
 
 
 def is_delimiter(token: str):
@@ -123,20 +145,48 @@ def is_delimiter(token: str):
     else:
         return False, None
 
-  
-def is_boolean_operator(token: str):
-    pass
-    
-
 def is_litnum(token: str):
-    pass
+    if token[0].isnumeric() or token[0] == '-':
+        golabi = get_token_until_delspop(token)
 
+        if is_hex(golabi):
+            return True, "T_HexaDecimal", golabi
+        elif golabi[0] == '-' and golabi[1:].isnumeric():
+            return True, "T_Decimal", golabi
+        elif golabi.isnumeric():
+            return True, "T_Decimal", golabi
+    else:
+        return False, None, None
 
 def is_litstring(token: str):
-    pass
+    if token[0] == "'":
+        if token[1] == "\\" and token[3] == "'":
+            if token[2] == "'":
+                return True, "T_Char", token[:4]
+            elif token[2] == "\\":
+                return True, "T_Char", token[:4]
+        elif token[2] == "'":
+            return True, "T_Char", token[:3]
+
+    elif token[0] == '"':
+        index = 0
+        for i in range(1, len(token)):
+            if token[i] == '"':
+                if token[i - 1] == '\\':
+                    continue
+                index = i
+                break
+        golabi = token[:index + 1]
+
+        return True, "T_String", golabi
+
+
+    else: 
+        return False, None, None
+
 
 def is_keyword(token: str):
-    golabi = get_token_until_delsp(token)
+    golabi = get_token_until_delspop(token)
     
     if golabi == "bool":
         return True, "Bool"
@@ -200,7 +250,7 @@ def get_next_token(text):
 def get_tokens():
     tokens = []
     count = 0
-    for line in read_file_line("tests/test3.txt"):
+    for line in read_file_line("tests/test5.txt"):
         count += 1
         beg = 0
         while(beg < len(line)):
@@ -221,20 +271,30 @@ def get_tokens():
             elif is_identifier(line[beg:])[0]:
                 yield Token("T_ID", count, is_identifier(line[beg:])[1])
 
-                beg += len(is_identifier(line[beg:])[1])
-            elif is_operator(line[beg:]):
-                pass
-            elif is_boolean_operator(line[beg:]):
-                break
-            elif is_litnum(line[beg:]):
-                break
-            elif is_litstring(line[beg:]):
-                break
+                beg += len(is_identifier(line[beg:])[1]) - 1
+            elif is_operator(line[beg:])[0]:
+                operator = is_operator(line[beg:])[1]
+                token_name = get_token_name(operator)
+                
+                yield Token(token_name, count, operator)
+
+                beg += len(operator) - 1
+
+            elif is_litnum(line[beg:])[0]:
+                _ , token_name, number= is_litnum(line[beg:])
+                yield Token(token_name, count, number)
+
+                beg += len(number) - 1
+            elif is_litstring(line[beg:])[0]:
+                _ , token_name, word = is_litstring(line[beg:])
+                yield Token(token_name, count, word)
+                beg += len(word) - 1
             beg += 1
     return tokens
 
 
 if __name__ == "__main__":
     gen = get_tokens()
+
     for token in gen:
         print(token)
